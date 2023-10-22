eff17bd53a8aef775f7b101c90a21ad2179ef5a5
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-22 11:52:46 +0300

    add start leartning func

--- app/src/main/java/com/memmorise/app/interective/learning/LearningStarter.java
+++ app/src/main/java/com/memmorise/app/interective/learning/LearningStarter.java
@@ -15,11 +15,17 @@ public class LearningStarter {
     private DiskWorker dw;
     private ClientTach ct;
     private LearnMap learnMap;
+    private FirstLevel firstLevel;
+    private SecondLevel secondLevel;
+    private TherdLevel therdLevel;
 
 
     public LearningStarter() {
         user = User.getInstance();
         dw = new DiskWorker();
+        firstLevel = new FirstLevel();
+        secondLevel = new SecondLevel();
+        therdLevel = new TherdLevel();
         ct = ClientTach.getInstance();
     }
 
@@ -37,6 +43,28 @@ public class LearningStarter {
     private void learnProces(int libIndex) throws IOException {
         Library curLib = user.getLibraries().get(libIndex -1);
         learnMap = new LearnMap();
-        learnMap.setMap(curLib, 0);
+        System.out.println("Set level of novelage in this library");
+        System.out.println("Where 0 -> is you don't now this words");
+        System.out.println("1 -> you actually now this words but not good");
+        System.out.println("2 -> you now good this words");
+        int level = ChecksUtils.writeInt(0, 2);
+        System.out.println("Ok, and set please size of pack of words then you will learn for each step");
+        int packSize = ChecksUtils.writeInt(1, 30);
+        System.out.println("Ok, good");
+        learnMap.setMap(curLib, level);
+        switch (level) {
+            case 0 -> {
+                firstLevel.init(curLib, learnMap, packSize);
+                firstLevel.doProcess();
+            }
+            case 1 -> {
+                secondLevel.init(curLib, learnMap, packSize);
+                secondLevel.doProcess();
+            }
+            case 2 -> {
+                therdLevel.init(curLib, learnMap, packSize);
+                therdLevel.doProcess();
+            }
+        }
     }
 }
--- app/src/test/java/com/memmorise/app/learning/LearnUtilTest.java
+++ app/src/test/java/com/memmorise/app/learning/LearnUtilTest.java
@@ -1,42 +1,5 @@
 package com.memmorise.app.learning;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.memmorise.app.interective.learning.LearnMap;
-import com.memmorise.app.interective.learning.util.LearnUtil;
-
 public class LearnUtilTest {
 
-    static LearnMap<Integer, String> help;
-
-
-    @BeforeAll
-    static void initHelp() {
-        help = new LearnMap<>();
-        List<String> words = List.of ("Hello", "Bye", "Cat", "Dog", "Perrot", "Animal", "Man", "Women", "Child", "Leazy");
-        List<String> trans = List.of("Привет, хай", "Пока, бай", "Кот, кошка", "Пес, собака", "Папугай", "Животное, зверь"
-                , "Мужчина", "Женщина", "Ребенок", "Лунивый, лентяй");
-
-        for (int i = 0; i < words.size(); i++) {
-            help.put(i, words.get(i), trans.get(i));
-        }
-    }
-
-
-    @Test
-    void getRounmFourTest() {
-
-        String tranlations = "Кот, кошка";
-
-        List<String> randomFour = LearnUtil.getFourRandmTranslations(help, tranlations);
-
-        for(String s : randomFour) {
-            assertTrue(help.conteins(s));
-        }
-    }
 }
